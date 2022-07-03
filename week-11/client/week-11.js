94ff285a08e078a9ee61e4e72cbc4057fafa9439
sol21002@byui.edu <85121697+Alainsol@users.noreply.github.com>
2022-07-03 08:34:03 +0700

    update

--- week-11/client/week-11.js
+++ week-11/client/week-11.js
@@ -1,19 +1,4 @@
-/*{
-  "name": "auth-demo",
-  "version": "1.0.0",
-  "description": "",
-  "main": "server.js",
-  "scripts": {
-    "start": "json-server --watch ./database.json",
-    "start-auth": "node server.js"
-  },
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "json-server": "^0.15.0",
-    "jsonwebtoken": "^8.5.1"
-  }
-}*/import Auth from './Auth.js';
+import Auth from './Auth.js';
 import { Errors, makeRequest } from './authHelpers.js';
 // makeRequest('login', 'POST', {
 //   password: 'user1',
